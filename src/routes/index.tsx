da3a0d47b8e1a4d09d608457edbc3cb4c31a10da
rodrigo travessa <rodrigosigiliao@outlook.com>
2024-01-20 17:02:53 -0300

    feat: adicionado Material Ui e o Tema Claro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import Button from "@mui/material/Button";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 
@@ -5,7 +6,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 export const AppRoutes = () =>{
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" >Teste</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
 
         </Routes>
